5f32cf8ea5ea038d27b8a7b8358d4fba60952417
Alexey Shmelkov <ashmelkov@aligntech.com>
2019-05-27 08:05:52 +0300

    json + filtered data

--- src/App.js
+++ src/App.js
@@ -2,30 +2,26 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Joke from './Joke'
+import jokesData from './jokesData';
+
 
 function App() {
+  const jokeDataFiltered = jokesData.filter(
+    joke => {
+      if (joke.question){
+        return (joke.question).length > 50;
+      }
+    }
+  );
+  const jokeComponents = jokeDataFiltered.map(
+    joke => <Joke 
+    key = {joke.id} 
+    question = {joke.question} 
+    punchLine = {joke.punchLine} 
+    />);
   return (
     <div className='jokes'>
-      <Joke 
-      name = 'Сиськи'
-      question = 'xxx: Сиськи, пресс...xxx: Вот у меня парень хочет, чтобы у меня аура была большая.'
-      punchLine = 'xxx: А у меня от этих медитаций минус к защите'/>
-      <Joke 
-      name = 'Обязанности пешеходов'
-      question = {`xxx: При отсутствии тротуаров, пешеходных дорожек, 
-      велопешеходных дорожек или обочин,
-      а также в случае отсутствия шоссе, 
-      трасс, магистралей, узколеек, перегонов, 
-      просек, собачьих упряжек, плотов, барж и снегоступов 
-      пешеход один хрен`}
-      punchLine = 'обязан припиз@%#ить к 09.00 на работу, блеать!!!11'/>
-      <Joke 
-      name = 'Бонд'
-      question = {`Съёмки 25-го «Бонда» приостановили из-за травмы Дэниела Крэйга
-      xxx: Только сегодня читал, что у них еще сценарий не дописан, а тут еще и травма...`}
-      punchLine = 'yyy: Зато сценарий допишут :D'/>
-      <Joke 
-      punchLine = 'It’s hard to explain puns to kleptomaniacs because they always take things literally'/>
+      {jokeComponents}
     </div>
   );
 }
